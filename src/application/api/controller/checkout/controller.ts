1e39f793310725430696d2b04f391738145bd02b
Rustamxojayev Muhammadxoja <muhammadxojaofficial@gmail.com>
2025-05-11 18:35:46 +0500

    changed create link url

--- src/application/api/controller/checkout/controller.ts
+++ src/application/api/controller/checkout/controller.ts
@@ -1,22 +1,21 @@
-import {Controller, HttpStatus, Post, Req, Res, UseGuards} from "@nestjs/common";
-import {AuthGuard} from "app/application/api/guard";
-import {CheckoutCreateLinkUseCase} from "app/application/usecases/checkout/create-link";
-import {JwtPayload, decode} from "jsonwebtoken"
-import {Response, Request} from "express";
-import {UserSubscriptionPlan, PaymentProcessorSubscription} from "app/domain";
+import { Controller, HttpStatus, Post, Req, Res, UseGuards } from "@nestjs/common";
+import { AuthGuard } from "app/application/api/guard";
+import { CheckoutCreateLinkUseCase } from "app/application/usecases/checkout/create-link";
+import { JwtPayload, decode } from "jsonwebtoken";
+import { Response, Request } from "express";
+import { UserSubscriptionPlan, PaymentProcessorSubscription } from "app/domain";
 
 @UseGuards(AuthGuard)
 @Controller("checkout")
 export class CheckoutController {
-    constructor(private readonly checkoutCreateLinkUseCase: CheckoutCreateLinkUseCase) {
-    }
+    constructor(private readonly checkoutCreateLinkUseCase: CheckoutCreateLinkUseCase) {}
 
-    @Post("/create-link/starter")
+    @Post("/create-link/plus")
     async createCheckoutLinkStarter(
         @Req() request: Request,
         @Res() response: Response,
     ): Promise<void> {
-        const {userId, email, name} = decode(request.header("Token") as string) as JwtPayload;
+        const { userId, email, name } = decode(request.header("Token") as string) as JwtPayload;
 
         const checkoutUrl = await this.checkoutCreateLinkUseCase.execute(
             {
@@ -37,7 +36,6 @@ export class CheckoutController {
             },
         );
 
-        response.status(HttpStatus.OK).json({checkout_url: checkoutUrl});
+        response.status(HttpStatus.OK).json({ checkout_url: checkoutUrl });
     }
-
 }
